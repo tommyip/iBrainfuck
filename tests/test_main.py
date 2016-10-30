c4615e8bfdfe39760ee94209aefb1f2659afd146
Tommy Ip <hkmp7tommy@gmail.com>
2016-10-30 14:30:02 +0000

    Renamed test folder for better test discovery

---
+++ tests/test_main.py
@@ -0,0 +1,27 @@
+"""
+Pytest test module
+"""
+
+from ..main import parser, lexier
+
+
+def test_parser():
+    # Test source code from file
+    assert parser("tests/test.bf") == "+++++++++[>++++++++++<-]>+++++++."
+
+    # Test source code from string
+    # assert parser("comments +++++++++[>++++++ loop here ++++<-]>+++++++.") == \
+    #     "+++++++++[>++++++++++<-]>+++++++."
+
+
+def test_lexier():
+    # Test default
+    assert lexier("+++++++++[>++++++++++<-]>+++++++.") == \
+        ["+++++++++", "[>++++++++++<-]", ">+++++++."]
+
+    # Test loop at the end
+    assert lexier("+++++++++[>++++++++++<-][+++]") == \
+        ["+++++++++", "[>++++++++++<-]", "[+++]"]
+
+    # Test no code
+    assert lexier("") == []
